5b61fdd9edb90bf0bfea751c007649345bb53b13
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-03-08 16:43:53 +0530

    Simple java program structure
    Basic structure of a java program

---
+++ Exercise0/src/exercise0/Exercise0.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise0;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Exercise0 {
+    public static void main(String args []){
+        System.out.println("I Love Java. Java is easy to learn");
+    }
+}
