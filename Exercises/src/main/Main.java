a7eed03fc3641abfce52ea253135180e09a5b06e
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-03-10 19:57:47 +0530

    Exercise 9 - Arrays

--- Exercises/src/main/Main.java
+++ Exercises/src/main/Main.java
@@ -6,25 +6,17 @@
 package main;
 
 
-import java.util.Scanner;
+
 /**
  *
  * @author MaNa
  */
 public class Main {
     public static void main(String args[]){
-       Scanner input = new Scanner(System.in);
-       int counter = 0, index = 0, marks = 0, total = 0;
        
-       while(counter < 10){
-           System.out.println("Enter index");
-           index = input.nextInt();
-           System.out.println("Enter marks");
-           marks = input.nextInt();
-           total = total + marks;
-           counter++;
-       }
-       double avg = total/10;
-       System.out.println("Average marks " + avg);
+        int numArray[] = {2,4,6,8,10};
+       for(int k : numArray){
+           System.out.println(k);
+       } 
     }
        }
