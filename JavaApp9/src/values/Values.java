b2c5c50cbad6d758da2be327c991173704b8637b
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-04-16 14:01:10 +0530

    abstract methods and classes

---
+++ JavaApp9/src/values/Values.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package values;
+
+/**
+ *
+ * @author MaNa
+ */
+abstract public class Values {
+    
+    abstract void calculate(int x, int y);
+}
