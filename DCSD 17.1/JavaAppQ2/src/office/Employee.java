a43eb64c9720b79e7c668db2ff2de2653dabf397
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-04-29 13:30:29 +0530

    Question 2 part a

---
+++ DCSD 17.1/JavaAppQ2/src/office/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package office;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Employee {
+    public void display(int eNumber, String eName){
+        System.out.println("Employee Number" + eNumber);
+        System.out.println("Employee Name" + eName);
+    }
+}
