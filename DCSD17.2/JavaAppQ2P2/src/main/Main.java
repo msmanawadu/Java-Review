e2f3e2da69d3519767c21fad81405a86b0ab18bd
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-05 14:06:40 +0530

    Q2 part 2

---
+++ DCSD17.2/JavaAppQ2P2/src/main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Main {
+    public static void main(String args []){
+        
+        int i = 1;
+        
+        while(i <= 20){
+            if(i%2 == 1){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
