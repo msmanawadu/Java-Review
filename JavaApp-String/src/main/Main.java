7cf9bae276088403dc81a0928e7fc45af74d0fff
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-08 07:41:27 +0530

    String Manipulation

---
+++ JavaApp-String/src/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Main {
+    public static void main(String args []){
+        
+        String s1 = new String("NIBM Diploma");
+        
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+        
+        String s3 = s1.toUpperCase();
+        System.out.println(s3);
+    }
+}
