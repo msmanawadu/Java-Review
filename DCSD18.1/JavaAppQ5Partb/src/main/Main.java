6c0cc6a16edbb884c643f2a0d98c1d8272cb8da1
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-08 09:05:36 +0530

    String Manipulation

---
+++ DCSD18.1/JavaAppQ5Partb/src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Main {
+    public static void main(String args []){
+        String s1 = new String("SOCNIBM");
+        char a = s1.charAt(3);
+        System.out.println(a);
+        
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+    }
+}
