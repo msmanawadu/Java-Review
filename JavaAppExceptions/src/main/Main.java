c62288ba46f7332b5f82bd523d068a93530e831f
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-13 14:05:56 +0530

    Exception handling done

---
+++ JavaAppExceptions/src/main/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class Main {
+    public static void main(String args []){
+        doStuff();
+    }
+    
+    static void doStuff(){
+        try{
+            doMoreStuff();
+        }
+        catch(ArithmeticException e){
+            System.out.println("Cannot divide by 0");
+        }
+        
+    }
+    
+    static void doMoreStuff(){
+        int x = 5/0;
+        System.out.println(x);
+    }
+}
