870d33e125cf3bf57b991f55373f100481520cbe
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-18 10:03:14 +0530

    DLL - add last to append a node at the end

---
+++ DoublyLinkedList/DoublyLinkedList-Add Last/src/main/DoublyLinkedList.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class DoublyLinkedList {
+    static Node head; //head pointer of the list
+    
+    //linked list Node. This inner class is made static so that main() can access it.
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        //constructor overloading
+        Node(int d){
+            data = d;
+            next = null;
+            prev = null;
+        }
+        Node(int d, Node p, Node n){
+            data = d;
+            prev = p;
+            next = n;
+        }
+    }
+    
+    //main method to create a doubly linked list with 3 nodes
+    public static void main(String args []){
+        head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        head.next = second;
+        second.prev = head;
+        second.next = third;
+        third.prev = second;
+        
+        display(head);
+        addLast(4);
+        display(head);
+    }
+    
+    //display function - traversing in both direction along the DLL
+    static void display(Node currentNode){
+        Node tail = null;
+        while(currentNode != null){
+            System.out.println("Data: " + currentNode.data);
+            tail = currentNode;
+            currentNode = currentNode.next;
+        }
+        currentNode = tail;
+        while(currentNode != null){
+            System.out.println("Descending Order Data: " + currentNode.data);
+            currentNode = currentNode.prev;
+        }
+    }
+
+    //addLast function to create a node and appends it at the end of the list.
+    public static void addLast(int item){
+        //check whether the DLL is empty
+        if(head == null){
+            addFirst(item);
+        }
+        else {
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new Node(item, temp, null);
+        }
+    }
+
+    //addFirst function to create a node and prepends it at the beginning of the list.
+    public static void addFirst(int item){
+        Node temp = head;
+        head = new Node(item, null, head);
+        temp.prev = head;
+    }
+}
