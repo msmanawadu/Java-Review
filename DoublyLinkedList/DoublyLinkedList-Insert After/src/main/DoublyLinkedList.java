4e6dd7d88593b6012e947854b12dd76583e0157e
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-05-18 11:05:19 +0530

    insert after function

---
+++ DoublyLinkedList/DoublyLinkedList-Insert After/src/main/DoublyLinkedList.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MaNa
+ */
+public class DoublyLinkedList {
+    static Node head; //head pointer of the list
+    
+    //linked list Node. This inner class is made static so that main() can access it.
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        //constructor overloading
+        Node(int d){
+            data = d;
+            next = null;
+            prev = null;
+        }
+        Node(int d, Node p, Node n){
+            data = d;
+            prev = p;
+            next = n;
+        }
+    }
+    
+    //main method to create a doubly linked list with 3 nodes
+    public static void main(String args []){
+        head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        head.next = second;
+        second.prev = head;
+        second.next = third;
+        third.prev = second;
+        
+        display(head);
+        insertAfter(3, 4);
+        display(head);
+    }
+    
+    //display function - traversing in both direction along the DLL
+    static void display(Node currentNode){
+        Node tail = null;
+        while(currentNode != null){
+            System.out.println("Data: " + currentNode.data);
+            tail = currentNode;
+            currentNode = currentNode.next;
+        }
+        currentNode = tail;
+        while(currentNode != null){
+            System.out.println("Descending Order Data: " + currentNode.data);
+            currentNode = currentNode.prev;
+        }
+    }
+
+    /*insertAfter function - find the node containing "key" data value and insert a new node 
+    after it*/
+    public static void insertAfter(int keyValue, int toInsert){
+        Node temp = head;
+        while(temp != null && temp.data != keyValue){
+            temp = temp.next;
+        }
+        if(temp != null){
+            Node nextRef = temp.next;
+            temp.next = new Node(toInsert, temp, nextRef);
+            //nextRef.prev = temp.next;
+        }
+    }
+}
